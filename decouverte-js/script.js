b9bedea4916e8a8e02bc821cc8398ee59e3da2c4
joachimzadi <gitprojects@j4ltechnologies.com>
2020-08-20 09:34:09 +0200

    Complement TP

---
+++ decouverte-js/script.js
@@ -0,0 +1,114 @@
+/*
+let prenom ;
+prenom = 'Joachim';
+console.log(prenom);
+alert(prenom);
+
+prenom = "Ange";
+
+// Le Kamel case
+let maVariable = 23;
+
+let _uneVariable = "Autre variable";
+*/
+
+//let prenom = prompt("Entrez votre prenom :")
+//console.log("type de prenom = " + typeof prenom);
+
+//let age = prompt("Entrez votre âge :");
+//age = Number(age);
+//console.log("type de age = " + typeof age);
+/*
+let uneChaineDeCaractere = "Lassina";
+console.log(typeof uneChaineDeCaractere);
+
+let unNumber =  23.09;
+console.log(typeof unNumber);
+
+let unBoolean = true;
+console.log(typeof unBoolean);
+
+let prenom;
+console.log(typeof prenom);
+*/
+
+//LES BOOLEANS
+//============
+
+// let estVrai = true;
+// let estFaux = false;
+//
+// console.log(!estVrai);
+// console.log(!estFaux);
+
+// console.log("a" < "b");
+// console.log("a" < "B");
+
+//console.log(3  == '3');
+
+
+// let condition = false;
+// condition = "a";
+// condition = undefined;
+//
+// if(condition){
+//     console.log("Condition verifiée !!!");
+// }
+
+/*let age = Number(prompt("Entrez votre age :"));
+
+if (age < 18) {
+    console.log("Division mineure");
+} else if (age >= 18 && age < 25) {
+    console.log("Division normale");
+}else {
+    console.log("Super division");
+}*/
+
+// // BOUCLE TANT-QUE
+// let compteur = 11;
+//
+// while (compteur <= 10) {
+//     console.log(compteur);
+//     compteur = compteur + 1;
+// }
+//
+//
+// // BOUCLE FAIRE -TANT-QUE
+// let compteur = 11;
+//
+// do {
+//     console.log(compteur);
+//     compteur = compteur + 1;
+// }while (compteur <= 20)
+
+// BOUCLE POUR
+
+// for (let i = 1; i <= 10; i = i + 1) {
+//     console.log(i);
+// }
+
+// let tabMois = [];
+//
+// tabMois.push("Janvier");
+// tabMois.push("Fevrier");
+// tabMois.push("Mars");
+// tabMois.push("Avril");
+// tabMois.push("Mai");
+// tabMois.push("Juin");
+//
+// console.log(tabMois);
+//
+// console.log("Taille = " + tabMois.length);
+// console.log("Element N° = " + tabMois[0]);
+
+//Parcours du tableau
+// for (let i = 0; i < tabMois.length; i = i + 1) {
+//     console.log(tabMois[i]);
+// }
+
+let uneChaineDeCaracteres = "Quel delice qu'est l'Algo avec JS";
+console.log(uneChaineDeCaracteres.length);
+console.log(uneChaineDeCaracteres[5]);
+
+
